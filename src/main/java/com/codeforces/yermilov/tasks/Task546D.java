bc7868760e475de79089a0458c453a05fdbb2d15
Yaroslav Yermilov <yermilov17@gmail.com>
2017-06-16 01:58:12 +0300

    564-D: implement solution

--- src/main/java/com/codeforces/yermilov/tasks/Task546D.java
+++ src/main/java/com/codeforces/yermilov/tasks/Task546D.java
@@ -27,7 +27,30 @@ public class Task546D {
         printWriter.close();
     }
 
+    private static final int MAX_AB = 5_010_000;
+
+    private final int[] dt;
+    private final int[] dtf;
+
+    Task546D() {
+        dt = new int[MAX_AB];
+        for (int i = 2; i < MAX_AB; i++) {
+            if (dt[i] == 0) {
+                dt[i] = 1;
+                for (int j = 2 * i; j < MAX_AB; j += i) {
+                    dt[j] = dt[j / i] + 1;
+                }
+            }
+        }
+
+        dtf = new int[MAX_AB];
+        dtf[1] = dt[1];
+        for (int i = 2; i < MAX_AB; i++) {
+            dtf[i] = dtf[i - 1] + dt[i];
+        }
+    }
+
     int solve(int a, int b) {
-        return 0;
+        return dtf[a] - dtf[b];
     }
 }
--- src/test/java/com/codeforces/yermilov/tasks/Task546DTest.groovy
+++ src/test/java/com/codeforces/yermilov/tasks/Task546DTest.groovy
@@ -12,8 +12,12 @@ class Task546DTest extends Specification {
         task546D.solve(a, b) == expected
 
         where:
-        a | b || expected
-        3 | 1 || 2
-        6 | 3 || 5
+        a         | b         || expected
+        3         | 1         || 2
+        6         | 3         || 5
+        1         | 1         || 0
+        5         | 5         || 0
+        5_000_000 | 5_000_000 || 0
+        5_000_000 | 1         || 18703742
     }
 }
